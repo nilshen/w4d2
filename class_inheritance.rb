ecd3877b983da7aeed4752004ad20ddf8c8b0352
Michael Chen <michael.chen.1120@gmail.com>
2022-04-19 14:41:57 -0400

    Finish class inheritance

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -1,51 +1,64 @@
+require 'byebug'
 class Employee
+    attr_reader :name, :salary, :title, :boss
 
     def initialize(name, salary, title, boss=nil)
-        @name, @salary, @title, @boss = name, salary, title, boss    
-        @direct_reports = []
+        @name, @salary, @title = name, salary, title
+        @boss = boss 
+        @boss.employees << self if !@boss.nil?
     end
 
     def bonus (multiplier)
         @salary * multiplier
     end
 
-    attr_reader :name,:salary,:title,:boss
 end
-
-
-class Associate < Employee
-
-
-end
-
 
 
 class Manager < Employee
+    attr_accessor :employees
 
     def initialize (name, salary, title, boss=nil)
         super
+        @employees = []
     end
 
-    def direct_reports
-        Employee.employee_list.each do |el|
-            if el.boss == self.name
-                @direct_reports << el
+    def bonus(multiplier)
+        count = 0
+        #debugger
+        @employees.each do |employee|
+            count += employee.salary
+            if employee.is_a?(Manager)
+                employee.employees.each do |em|
+                    count += em.salary
+                end
             end
         end
+        count * multiplier
     end
 
-attr_reader :direct_reports
+    #def total_salary(name)
+    #debugger
+    #total_salary = 0
+    #self.employees.each do |employee|
+    #    if employee.is_a?(Manager)
+    #        total_salary += self.bonus + total_salary(self.employees)
+    #    else 
+    #        total_salary += self.bonus
+    #    end
+    #end
+    #total_salary
 end
 
-def total_salary(name)
-    total_salary = 0
-    if self.is_a?(Associate)
-        total_salary += self.bonus
-    else 
-        total_salary += self.salary + total_salary()
-    end
-    total_salary
-end
+    #def direct_reports
+    #    Employee.employee_list.each do |el|
+    #        if el.boss == self.name
+    #            @direct_reports << el
+    #        end
+    #    end
+    #end
+
+
 
 
 # class Employee_list
@@ -59,15 +72,15 @@ end
 # end
 
 
-p ned = Manager.new("ned", 1000000,"founder")
-p darren = Manager.new("darren", 78000,"TA Manager","Ned")
-p shawna = Associate.new("shawna", 12000, "TA", "Darren")
-p david = Associate.new("david", 10000, "TA", "Darren")
+ned = Manager.new("Ned", 1000000,"founder")
+darren = Manager.new("Darren", 78000,"TA Manager",ned)
+shawna = Employee.new("Shawna", 12000, "TA", darren)
+david = Employee.new("David", 10000, "TA", darren)
 puts "-------------------------------------------------------"
 # p Employee.employee_list
 puts "-------------------------------------------------------"
-# p darren.direct_reports
-
-# ned.bonus(5) # => 500_000
-# darren.bonus(4) # => 88_000
-# p david.bonus(3) # => 30_000
+p shawna.boss
+p darren.boss
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+ p david.bonus(3) # => 30_000
