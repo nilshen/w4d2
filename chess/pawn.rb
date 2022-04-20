75ad5c577d90355b84a8b3c987e1bd58a1e3d584
nilshen <linshen88@gmail.com>
2022-04-20 16:03:05 -0400

    w4d2_pawn

--- chess/pawn.rb
+++ chess/pawn.rb
@@ -1,7 +1,8 @@
 require_relative 'piece'
+require 'byebug'
 
 class Pawn < Piece
-attr_reader :sym,:color, :board, :pos
+    attr_reader :sym,:color, :board, :pos
 
     def initialize (color = nil, board = nil, pos = nil)
         @color = color
@@ -12,5 +13,25 @@ attr_reader :sym,:color, :board, :pos
         @sym = :♟ if self.color == :black 
     end
 
+
+    def moves
+        move_dirs = []
+        moves = []
+
+        if self.pos[0] == 1 || self.pos[0] == 6
+            move_dirs = [[1,0], [2,0]] if self.color == :white
+            move_dirs = [[-1,0], [-1,0]] if self.color == :black
+        else
+            move_dirs << [1, 0] if self.color == :white
+            move_dirs << [-1,0] if self.color == :black
+        end
+        # debugger
+        
+        move_dirs.each do |dx, dy|
+            moves << [ pos[0]+ dx, pos[1]+ dy ]
+        end
+
+        moves
+    end
 
 end
